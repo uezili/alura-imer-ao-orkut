029e7c31c0f4186a133e0fa577c8b0fc2ad086c8
uezili <weslmaciel@gmail.com>
2021-08-17 07:59:45 -0300

    globalstyle

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu } from '../src/lib/ConponentsAlura';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const ProfileSidebar = (props) => {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'uezili'
+
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho',
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div classNAme="welcomeArea" style={{ gridArea: "welcomeArea"}}>
+          <Box>
+            Bem vindo 
+          </Box>
+        </div>
+        <div calssName="profileRelationsArea" style={{ gridArea: "profileRelationsArea"}}>
+          <Box clan>
+            <h2>
+              Pessoas da comunidades
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((current) => {
+                return (
+                  <a href={`/users/${current}`}>
+                      <img src={`https://github.com/${current}.png`} />
+                      <span>{current}</span>
+                    </a>
+                )
+              })};
+            </ul>
+          </Box>
+        </div>
+
+      </MainGrid>
+    </>
+  )
 }
